24d4b80ba9038694f894b39f6490c9987eb5c3e5
Goga Hayrapetyan <hayrapg@gmail.com>
2018-11-26 18:59:23 +0100

    navbar and stuff added

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,14 @@
+class CarsController < ApplicationController
+
+  def index
+    @cars = Car.all
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+end
