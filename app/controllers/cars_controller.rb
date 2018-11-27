5a3a598833e3eaa89361911a919efa380be2f506
Goga Hayrapetyan <hayrapg@gmail.com>
2018-11-27 16:55:35 +0100

    committing changes

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -19,14 +19,14 @@ class CarsController < ApplicationController
     end
   end
 
-    def search
-      @cars = Car.all
-      if params[:search]
-        @cars = Car.search(params[:search]).order("created_at DESC")
-      else
-        @cars = Car.all.order('created_at DESC')
-      end
-    end
+    # def search
+    #   @cars = Car.all
+    #   if params[:search]
+    #     @cars = Car.search(params[:search]).order("created_at DESC")
+    #   else
+    #     @cars = Car.all.order('created_at DESC')
+    #   end
+    # end
 
   private
 
