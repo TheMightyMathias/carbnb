5eadabd6469147767376feec5686a83b327ce03e
Goga Hayrapetyan <hayrapg@gmail.com>
2018-11-27 18:23:09 +0100

    committing searchbar

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -19,24 +19,17 @@ class CarsController < ApplicationController
     end
   end
 
-    # def search
-    #   @cars = Car.all
-    #   if params[:search]
-    #     @cars = Car.search(params[:search]).order("created_at DESC")
-    #   else
-    #     @cars = Car.all.order('created_at DESC')
-    #   end
-    # end
-# <<<<<<< searchbarcontinued
-#     def search
-#       @cars = Car.all
-#       if params[:search]
-#         @cars = Car.search(params[:search]).order("created_at DESC")
-#       else
-#         @cars = Car.all.order('created_at DESC')
-#       end
-#     end
-# =======
+    def search
+      raise
+
+      @cars = Car.all
+      if params[:search]
+        @cars = Car.where(location: params[:search]).order("created_at DESC")
+      else
+        @cars = Car.all.order('created_at DESC')
+      end
+    end
+
   def show
     set_car
     @car = Car.find(params[:id])
