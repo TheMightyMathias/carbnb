23bc921a6ebb718e643f5a723e88cde93c9ca29d
Douglas Bay <douglasbays@gmail.com>
2018-11-27 15:33:44 +0100

    Added more stuff to the car controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -19,9 +19,35 @@ class CarsController < ApplicationController
     end
   end
 
+  def show
+    set_car
+    @car = Car.find(params[:id])
+    @car.user = current_user
+  end
+
+  def edit
+    set_car
+  end
+
+  def update
+    set_car
+    @car.update(car_params)
+    redirect_to car_path(@car)
+  end
+
+  def destroy
+    set_car
+    @car.destroy
+    redirect_to root_path
+  end
+
   private
 
   def car_params
     params.require(:car).permit(:photos, :brand, :color, :year, :model, :location)
   end
+end
+
+def set_car
+  @car = Car.find(params[:id])
 end
